28360a5cedfa6b6703cfe3b235538bac8785d49b
eviai <76401346+eviai@users.noreply.github.com>
2021-08-01 15:10:56 +0100

    CSS Update
    - Search bar styling

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,10 @@ export default function Footer() {
   return (
     <div className="footerText">
       <h4>
-      <a href="https://github.com/eviai/weather_react.git" target="blank">Open source</a> code by{" "}
+        <a href="https://github.com/eviai/weather_app.git" target="blank">
+          Open source
+        </a>{" "}
+        code by{" "}
         <a href="https://instagram.com/evicodes.xyz" target="blank">
           Emily May
         </a>
